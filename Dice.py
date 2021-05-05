1618b652fdf192ece45ad8ac9e88074dfe42b19d
maksblink <blinkmaks777@gmail.com>
2021-05-05 16:58:35 +0200

    fin

--- Dice.py
+++ Dice.py
@@ -2,15 +2,26 @@ from random import randint
 
 
 def dice(code: str):
+    """Calculate dice roll from dice pattern.
+
+    :param str code: dice pattern ex. `7D12-5`
+
+    :rtype: str
+    :dice roll value for proper dice pattern
+    """
     dices = [3, 4, 6, 8, 10, 12, 20, 100]
     try:
         x = int(code[: code.index("D")])
-        y = int(code[code.index("D") + 1: code.index("+")])
-        z = int(code[code.index("+") + 1:])
+        if "+" in code:
+            y = int(code[code.index("D") + 1: code.index("+")])
+            z = int(code[code.index("+") + 1:])
+        else:
+            y = int(code[code.index("D") + 1: code.index("-")])
+            z = int(code[code.index("-") + 1:])
     except ValueError as ve:
         print(ve)
         return None
-    if not y in dices:
+    if y not in dices:
         print("There is no such dice!")
         return None
     result = 0
@@ -19,5 +30,5 @@ def dice(code: str):
     return result + z
 
 
-throw = dice("3D6+1")
+throw = dice("7D12-5")
 print(throw)
